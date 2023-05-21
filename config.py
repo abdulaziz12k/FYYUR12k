48ee2efb6943c979a6c12db6f99c03c71eee8b4b
abdulaziz12k <abdullaziz12k@gmail.com>
2023-05-21 03:50:45 +0300

    -

--- config.py
+++ config.py
@@ -2,7 +2,10 @@ import os
 SECRET_KEY = os.urandom(32)
 basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
-# Connect to the database
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-#db connection
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:azoozyh1122@localhost:5432/project'
+#db connection - insted of.. SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:azoozyh1122@localhost:5432/project'
+DB_HOST = os.getenv('DB_HOST', 'localhost:5432')
+DB_USER = os.getenv('DB_USER', 'postgres')
+DB_PASSWORD = os.getenv('DB_PASSWORD', 'azoozyh1122')
+DB_NAME = os.getenv('DB_NAME', 'project')
+DB_PATH = 'postgresql+psycopg2://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
